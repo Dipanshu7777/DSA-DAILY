fa83faef2a9e5d38d39866a159d8d332f3fb22a7
Dipanshu <kumardipanshu7777@gmail.com>
2025-05-14 18:07:13 +0530

    Added Day01 folder with problem1.cpp

---
+++ Day01/prob1.cpp
@@ -0,0 +1,40 @@
+/*
+Given an integer n. Return the nth row of the following look-and-say pattern.
+1
+11
+21
+1211
+111221
+Look-and-Say Pattern:
+
+To generate a member of the sequence from the previous member, read off the digits of the previous member, counting the number of digits in groups of the same digit. For example:
+
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+1211 is read off as "one 1, one 2, then two 1s" or 111221.
+111221 is read off as "three 1s, two 2s, then one 1" or 312211*/
+
+class Solution {
+  public:
+    string countAndSay(int n) {
+    if (n == 1) return "1";
+
+    string result = "1";
+    for (int i = 2; i <= n; ++i) {
+        string current = "";
+        int count = 1;
+        for (int j = 1; j < result.size(); ++j) {
+            if (result[j] == result[j - 1]) {
+                count++;
+            } else {
+                current += to_string(count) + result[j - 1];
+                count = 1;
+            }
+        }
+        current += to_string(count) + result.back();  // for the last group
+        result = current;
+    }
+    return result;
+    }
+};
